789054879e2c07d9c95c9d6bcb1cf48c3a27c5dc
unknown <anhthanh1996vp@gmail.com>
2019-05-30 18:34:40 +0700

    commit two

--- lession1/App.js
+++ lession1/App.js
@@ -6,8 +6,11 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Avatar } from './src/components';
+
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,12 +23,16 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+      <Avatar />
+    )
+
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>Welcome to React Native!</Text>
+    //     <Text style={styles.instructions}>To get started, edit App.js</Text>
+    //     <Text style={styles.instructions}>{instructions}</Text>
+    //   </View>
+    // );
   }
 }
 
---
+++ lession1/src/components/Avatar/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+export default class index extends Component {
+    render() {
+        const { width, height, borderRadius, backgroundColor } = this.props;
+        return <View style={{
+            width, height, borderRadius, backgroundColor
+        }} />
+    }
+}
