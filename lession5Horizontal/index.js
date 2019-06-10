607fc827fae586685ff57af88d2c6efce1ad33f0
NAT <nat@AP24HnoMacBook-Pro.local>
2019-06-11 02:53:00 +0700

    change

--- lession5Horizontal/index.js
+++ lession5Horizontal/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
+import App from './App';
 import ScrollViewComponent from './components/ScrollViewComponent';
 
 import {name as appName} from './app.json';
